ee76b2ff98d0458b540d695900fa4d6e2b15de7a
Caio Phelipe <caio.dev.ph@gmail.com>
2024-10-30 10:29:16 -0300

    teste de requisição da nave Y-wing

--- swapi.spec.js
+++ swapi.spec.js
@@ -147,3 +147,27 @@ test('Deve receber msg erro, quando nave inexistente',async ()=> {
         detail: 'Not found'
     });
 });
+
+//-----------------------------------------------------------------------//
+test('',async ()=> {
+    const resposta = await request('https://swapi.dev/api').get('/starships/11/');
+// /starships/11/
+
+expect(resposta.status).toBe(200);
+expect(Number(resposta.body.cost_in_credits)).toBeGreaterThan(0);
+expect(Number(resposta.body.length)).toBeGreaterThan(0);
+expect(resposta.body.name).toBe('Y-wing');
+expect(resposta.body.consumables).toBe("1 week");
+});
+test('Mensagem erro de nave inexistente', async ()=>{
+
+});
+test('Deve receber msg erro, quando nave inexistente',async ()=> {
+    const resposta = await request('https://swapi.dev/api').get('/starships/9999');
+
+    expect(resposta.status).toBe(404);
+    expect(resposta.body.detail).toBe('Not found');
+    expect(resposta.body).toMatchObject({
+        detail: 'Not found'
+    });
+});
