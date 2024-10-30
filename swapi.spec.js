a8cbe6213ce7d8b51a57d7a703f9147407c3d58a
Caio Phelipe <caio.dev.ph@gmail.com>
2024-10-30 09:52:01 -0300

    Iniciando Projeto

---
+++ swapi.spec.js
@@ -0,0 +1,30 @@
+it ('1 + 1 deve ser igual a 2', ()=>{
+    const resultadoEsperado = 2;
+    expect(1+1).toBe(resultadoEsperado);
+});
+
+const request = require('supertest')
+
+test('',async ()=> {
+    const resposta = await request('https://swapi.dev/api').get('/people/1');
+ //   console.log(resposta.status);
+// console.log(resposta.body);
+expect(resposta.status).toBe(200);
+expect(resposta.body.films).toBeDefined();
+expect(resposta.body.vehicles.length).toBeGreaterThan(0);
+expect(resposta.body.name).toBe('Luke Skywalker');
+//expect(resposta.body.vehicles.length).toBeGreaterThan(999);
+});
+test('Mensagem erro de pessoa inexistente', async ()=>{
+
+});
+test('Deve receber msg erro, quando pessoa inexistente',async ()=> {
+    const resposta = await request('https://swapi.dev/api').get('/people/9999');
+
+    expect(resposta.status).toBe(404);
+    expect(resposta.body.detail).toBe('Not found');
+    expect(resposta.body).toMatchObject({
+        detail: 'Not found'
+    });
+//expect(resposta.body.vehicles.length).toBeGreaterThan(999);
+});
