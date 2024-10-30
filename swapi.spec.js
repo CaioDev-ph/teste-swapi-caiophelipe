389a8b443648bf3b6a053ce6ba2c9d5177a44271
Caio Phelipe <caio.dev.ph@gmail.com>
2024-10-30 10:04:10 -0300

    teste de requisição do personagem Yoda

--- swapi.spec.js
+++ swapi.spec.js
@@ -28,3 +28,27 @@ test('Deve receber msg erro, quando pessoa inexistente',async ()=> {
     });
 //expect(resposta.body.vehicles.length).toBeGreaterThan(999);
 });
+//-----------------------------------------------------------------------//
+test('',async ()=> {
+    const resposta = await request('https://swapi.dev/api').get('/people/20');
+// /people/20
+
+expect(resposta.status).toBe(200);
+expect(resposta.body.films).toBeDefined();
+expect(resposta.body.species).toBeDefined();
+expect(resposta.body.name).toBe('Yoda');
+expect(resposta.body.height).toBe("66");
+});
+test('Mensagem erro de pessoa inexistente', async ()=>{
+
+});
+test('Deve receber msg erro, quando pessoa inexistente',async ()=> {
+    const resposta = await request('https://swapi.dev/api').get('/people/9999');
+
+    expect(resposta.status).toBe(404);
+    expect(resposta.body.detail).toBe('Not found');
+    expect(resposta.body).toMatchObject({
+        detail: 'Not found'
+    });
+});
+
